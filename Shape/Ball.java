e22da48cf23537ed3702c38af8a5b89143d66af4
mitchellsha <mitchellsha@gmail.com>
2014-04-23 15:32:58 -0500

    Ball updated

--- Shape/Ball.java
+++ Shape/Ball.java
@@ -1,3 +1,4 @@
+package Shape;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
@@ -6,29 +7,29 @@ import com.threed.jpct.CollisionEvent;
 import com.threed.jpct.CollisionListener;
 import com.threed.jpct.Object3D;
 import com.threed.jpct.Primitives;
+import com.threed.jpct.SimpleVector;
 import com.threed.jpct.World;
 
 public class Ball extends Object3D implements CollisionListener{
 	private static World world;
 	private float x, y, z;
+	private SimpleVector ballVector;
 	private float speedX, speedY, speedZ;
 	private double velocityX, velocityY, velocityZ;
 	private float radius;
-	private final double gravity = 9.8;
+	private final double g = 9.8;
 	private double mass;
 	private Color color;
 	float ballScale;
 	private double heading;
 	static Object3D myBall;
+
 	
-	public Ball(int resolution, float scale, float size, float direciton, double weight){
+	public Ball(SimpleVector vector, int resolution, float scale, float size, float direciton, double weight){
 		super(myBall);
 		ballScale = scale;
 		mass = weight;
-		speedX = 10;
-		speedY = 10;
-		speedZ = 10;
-		
+		ballVector = vector;
 		
 		radius = size;
 		heading = Math.toRadians(direciton);
@@ -37,13 +38,11 @@ public class Ball extends Object3D implements CollisionListener{
 		velocityY = speedY*heading;
 		velocityZ = speedZ*heading;
 		
-		
-
 		myBall = Primitives.getSphere(resolution, scale);
 		
 	}
 	
-	public void move(){
+	public void collisionMove(){
 		//check collision detection with all other balls in arena
 		//check collision detection with walls
 		//check collision detection with person
@@ -51,33 +50,47 @@ public class Ball extends Object3D implements CollisionListener{
 		double ballMinY = 0;
 		double ballMinZ = 0;
 		
-		x += speedX;
-		y += speedY;
-		z += speedZ;
-		
-		if(x <= ballMinX){
-			
-		} else if(y < ballMinY){
-			
-		} else if(z < ballMinZ){
-			
+		if(x <= ballMinX ){//if ball hits the x walls
+			ballVector.x = flipSign(ballVector.x);
+		} else if(y < ballMinY ){//if ball hits the floor... and ceiling?
+			ballVector.y = flipSign(ballVector.y);
+		} else if(z < ballMinZ){//if ball hits the y walls
+			ballVector.z = flipSign(ballVector.z);
+		}
+	}
+	
+	public void gravMove(){
+		velocityZ -= g/2;
+		ballVector.x -= velocityX;
+		ballVector.y -= velocityY;
+		ballVector.z += velocityZ;
+		velocityZ -= g/2;
+	}
+	
+	public float flipSign(float num){
+		if(num > 0){
+			return -num;
+		}
+		else{
+			return num;
 		}
 	}
 	
 	public void draw(Graphics g){
-		
-	}
-	
-	public float getSpeed(){
-		return (float)Math.sqrt(speedX*speedX + 
-				speedY*speedY + speedZ*speedZ);
+		//going to have to change this to 3D?
+		g.fillOval((int) ballVector.x,(int) ballVector.y,(int) radius/2,(int) radius/2);
 	}
 
 	@Override
 	public void collision(CollisionEvent e) {
 		// TODO Auto-generated method stub
 		Object3D obj = e.getSource();
-		
+		SimpleVector impactLocation = e.getFirstContact();
+		/*float impactX = impactLocation.x;
+		float impactY = impactLocation.y;
+		float impactZ = impactLocation.z;
+		*/
+		collisionMove();
 	}
 
 	@Override
